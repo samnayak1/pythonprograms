3de83b53adea55c2e11299995a2d6b5f95a71a7c
Samasth Nayak <samasthnayak1@gmail.com>
2023-03-25 20:53:01 +0530

    security changes 2

--- gettingreddit.py
+++ gettingreddit.py
@@ -1,6 +1,15 @@
 import praw
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET=os.getenv('CLIENT_SECRET')
+REDDITUSERNAME=os.getenv('REDDITUSERNAME')
+REDDITPASSWORD=os.getenv('REDDITPASSWORD')
+
+reddit=praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,username=REDDITUSERNAME,password=REDDITPASSWORD,user_agent='used to scrape data for educational purposes')
 subreddit=reddit.subreddit('soccer')
 
 hot_python=subreddit.hot(limit=5)
@@ -8,6 +17,5 @@ hot_python=subreddit.hot(limit=5)
 for submission in hot_python:
     if not submission.stickied:
         print('Title:{} ups:{} downs{} have we visited {} '.format(submission.title,submission.ups,submission.downs,submission.visited))
-
 
 
