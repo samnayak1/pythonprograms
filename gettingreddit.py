4b631d9c1964b9404221ab57ae7e1d87703a4876
Samasth Nayak <samasthnayak1@gmail.com>
2023-03-25 20:26:22 +0530

    security changes

--- gettingreddit.py
+++ gettingreddit.py
@@ -1,6 +1,5 @@
 import praw
 
-reddit=praw.Reddit(client_id='RxejSsjn5UJMVEV0mWjN5A',client_secret='ETfmg7y0_O0j2JcmYc3IDGVBP0mk9A',username='samnayak1',password='Grifter123',user_agent='used to scrape data for educational purposes')
 
 subreddit=reddit.subreddit('soccer')
 
